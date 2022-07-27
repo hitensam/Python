19139dbfdf09fa007d2c8a3385cdb8d6ad619ed9
Bhavay Luthra <48243256+bluthra999@users.noreply.github.com>
2022-07-27 18:40:36 +0530

    Update rock_paper.py

--- rock_paper.py
+++ rock_paper.py
@@ -2,9 +2,9 @@ import random
 container = ["""
     _______
 ---'   ____)
+      (_____)   
       (_____)
-      (_____)
-      (____)
+      (____)   
 ---.__(___)
 """, """
      _______
@@ -13,13 +13,26 @@ container = ["""
           _______)
          _______)
 ---.__________)
-""", """
+""","""
     _______
 ---'   ____)____
           ______)
        __________)
       (____)
 ---.__(___)
+""" , """
+
+ ~~I==I>
+
+""", """
+    _______
+---'   ____)____
+          ______)
+       __________)
+        (
+        (_____       
+          _____)
+       ______)
 """]
 def game():
     global comp_score, player_score;
@@ -27,9 +40,9 @@ def game():
         comp_choice = random.randrange(0,3,1)
             # a = print("ENTER A CHOICE ", input(int()));
         print(f"---------SCORE BOARD---------\nPLAYER : {player_score}\nCOMPUTER : {comp_score}\n")
-        print("SELECT A VALUE FROM 0, 1, 2\n0 : ROCK\n1 : PAPER\n2 : SCISSORS"); print("The computer has already chosen it's value\n")
+        print("SELECT A VALUE FROM 0, 1, 2 , 3 , 4 \n0 : ROCK\n1 : PAPER\n2 : SCISSORS\n3 : LIZARD\n4 :  SPOCK"); print("The computer has already chosen it's value\n")
         player = int(input("Enter a value ")); print("\n")
-        if (player == 0 or player == 1 or player == 2):
+        if (player == 0 or player == 1 or player == 2 or player==3 or player==4 ):
             print("YOU CHOOSE ", container[player])
             print("The computer chooses ", container[comp_choice]);
 
@@ -60,6 +73,62 @@ def game():
                     if(player==2 and comp_choice==1):
                         print("YOU WIN")
                         player_score +=1
+                if (player==2 and comp_choice==3 or player==3 and comp_choice==2):
+                    if(player==2 and comp_choice==3):
+                        print("you win :-)")
+                        player_score +=1
+                    if(player==3 and comp_choice==2):
+                        print("YOU loose :-(")
+                        comp_score +=1
+                
+
+                if (player==3 and comp_choice==4 or player==4 and comp_choice==3):
+                    if(player==3 and comp_choice==4):
+                        print("you win :-)")
+                        player_score +=1
+                    if(player==4 and comp_choice==3):
+                        print("YOU loose :-(")
+                        comp_score +=1
+                
+                if (player==4 and comp_choice==0 or player==0 and comp_choice==4):
+                    if(player==4 and comp_choice==0):
+                        print("you win :-)")
+                        player_score +=1
+                    if(player==0 and comp_choice==4):
+                        print("YOU loose :-(")
+                        comp_score +=1
+                
+                if (player==0 and comp_choice==3 or player==3 and comp_choice==0):
+                    if(player==0 and comp_choice==3):
+                        print("you win :-)")
+                        player_score +=1
+                    if(player==3 and comp_choice==0):
+                        print("YOU loose :-(")
+                        comp_score +=1
+                
+                if (player==1 and comp_choice==3 or player==3 and comp_choice==1):
+                    if(player==3 and comp_choice==1):
+                        print("you win :-)")
+                        player_score +=1
+                    if(player==1 and comp_choice==3):
+                        print("YOU loose :-(")
+                        comp_score +=1
+                
+                if (player==1 and comp_choice==4 or player==4 and comp_choice==1):
+                    if(player==1 and comp_choice==4):
+                        print("you win :-)")
+                        player_score +=1
+                    if(player==4 and comp_choice==1):
+                        print("YOU loose :-(")
+                        comp_score +=1
+
+                if (player==2 and comp_choice==4 or player==4 and comp_choice==2):
+                    if(player==4 and comp_choice==2):
+                        print("you win :-)")
+                        player_score +=1
+                    if(player==2 and comp_choice==4):
+                        print("YOU loose :-(")
+                        comp_score +=1
                 
         else:
             print("ENTER A VALID CHOICE")
@@ -75,11 +144,11 @@ def game():
 
 
 while (True):
-    choice = input("PRESS ANY KEY TO PLAY OR PRESS E TO EXIT FROM THE CODE "); print("\n")
+    choice = input("PRESS ANY KEY TO PLAY OR PRESS E TO EXIT FROM THE GAME:  "); print("\n")
     choice = choice.upper()
-    if (choice == 'E'):
+    if (choice == 'E' or choice == 'e'):
         print("EXITING FROM GAME")
-        break;
+        exit(0)
     else:
         comp_score, player_score = 0, 0; 
         game()
