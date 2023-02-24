0c41118b898bfd147b3d8ee39b825cc1c9904082
Hiten Samalia <30778907+hitensam@users.noreply.github.com>
2023-02-24 23:45:03 +0530

    Create pattern_codingNinjas.py

---
+++ patterns/pattern_codingNinjas.py
@@ -0,0 +1,11 @@
+N = 10
+
+for x in range(0, N):
+    calculated = N - x;"""print("\n\n",calculated,"\n\n")"""
+    spaces = N - calculated;"""print("\n\n",spaces,"\n\n")"""
+    for y in range(0, N):
+        if (spaces!=0):
+            print(" ", end = "");spaces = spaces - 1;continue;
+        if (calculated!=0):
+            print("*", end = "");calculated = calculated - 1;
+    print()
